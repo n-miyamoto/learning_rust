21efe5e67e61c6524ebc804c6044b9b4667d7bf3
naoya.miyamoto <naoya.miyamoto@fixstars.com>
2018-05-02 21:42:46 +0900

    first commit. add helloworld

---
+++ hello.rs
@@ -0,0 +1,9 @@
+
+fn main(){
+    // hello world.
+    println!("Hello World");
+
+    // print format.
+    println!("Todai is {month}/{day}",
+        month=4.to_string(), day=22.to_string() );
+}
